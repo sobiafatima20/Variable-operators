0930e17bc9387b255595988ca2f1a3e8bc8a99cd
Sobia Fatima <133638285+sobiafatima20@users.noreply.github.com>
2024-03-31 07:33:39 +0500

    Add files via upload

---
+++ class3.ts
@@ -0,0 +1,76 @@
+//Variables
+//Primtive Datatype
+//i. String
+//1. Let
+let firstName : string = "Sobia";
+console.log(firstName);
+
+//2. Const
+const gravity : number = 9.8;
+console.log(gravity);
+
+//3. Var
+var secondName : string = "Fatima";
+console.log(secondName);
+
+//ii. Number
+let num : number = 7;
+console.log(num);
+
+//iii. Boolean
+let isSobia : boolean = true;
+console.log(isSobia);
+
+//Template Literals
+let myName : string = "Sobia";
+console.log(`Hye ${myName}` );
+
+//Operators
+
+//1. Arithematic Operators
+//Addition
+let num1 : number = 7;
+let num2 : number = 8;
+console.log(num1 + num2);
+
+//Subtraction
+let num3 : number = 4;
+let num4 : number = 2;
+console.log(num3 - num4);
+
+//Multiplication
+let num5 : number = 2;
+let num6 : number = 4;
+console.log(num5 * num6);
+
+//Division
+let num7 : number = 10;
+let num8 : number = 2;
+console.log(num7 / num8);
+
+//Exponentiation
+let num9 : number = 2;
+let num10 : number = 3;
+console.log(num9 ** num10);
+
+//Modulus
+let num11 : number = 51;
+let num12 : number = 2;
+console.log(num11 % num12);
+
+//Coersion : add string and number
+let num13 = "7";
+let num14 = 7;
+console.log(num13 + num14);
+
+//Concatenation
+//add two  strings
+let num15 = "7";
+let num16 = "6";
+let result = num15 + num16
+console.log(result);
+
+//add two numbers
+let num17 = 7;
+let num18 = 6;
+console.log(num17 + num18);
